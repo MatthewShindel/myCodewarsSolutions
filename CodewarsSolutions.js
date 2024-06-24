4512c5a1082edc08fd1dda98339397955283b239
MatthewShindel <matthew@shindel.com>
2024-06-24 16:06:19 -0400

    added bouncingBall

--- CodewarsSolutions.js
+++ CodewarsSolutions.js
@@ -98,3 +98,18 @@ function alphabetPosition(text) {
   }
   return result.join(' ')
 }
+
+function bouncingBall(h,  bounce,  window) {
+  if(h <= 0 || bounce <= 0 || bounce >= 1 || window >= h){
+    return -1
+  }
+  let count = 0;
+  while (h > window){
+    count ++
+    h *= bounce;
+    if(h > window){
+      count ++
+    }
+  }
+  return count;
+}
