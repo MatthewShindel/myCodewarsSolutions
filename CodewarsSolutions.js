a76efaddc5249b6e68da546e5ea6d686cec2ced6
MatthewShindel <matthew@shindel.com>
2024-07-03 16:43:24 -0400

    added findEvenIndex

--- CodewarsSolutions.js
+++ CodewarsSolutions.js
@@ -197,3 +197,14 @@ var countBits = function(n) {
   }
   return count
 };
+
+function findEvenIndex(arr){
+  for(let i =0; i<= arr.length; i++){
+    let leftSum = arr.slice(0,i).reduce((acc,curr) => acc + curr, 0)
+    let rightSum = arr.slice(i+1).reduce((acc,curr) => acc + curr, 0)
+    if(leftSum === rightSum){
+      return i;
+    }
+  }
+  return -1
+}
