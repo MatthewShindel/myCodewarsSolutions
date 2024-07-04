a761fcc00ac86e4e9956afee7f5771f1bd43ade0
MatthewShindel <matthew@shindel.com>
2024-07-04 14:46:26 -0400

    added getMiddle

--- CodewarsSolutions.js
+++ CodewarsSolutions.js
@@ -261,3 +261,12 @@ function isPangram(string){
   }
   return letterSet.size === 26
 }
+
+function getMiddle(s){
+  let middle = Math.floor(s.length / 2);
+  if(s.length %2 === 0){
+    return s[middle-1] + s[middle];
+  }else{
+    return s[middle]
+  }
+}
