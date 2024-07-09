448316d3ae931faac7e598c7b865afa0320639b3
MatthewShindel <matthew@shindel.com>
2024-07-09 15:08:10 -0400

    added isIsogram

--- CodewarsSolutions.js
+++ CodewarsSolutions.js
@@ -397,3 +397,15 @@ var uniqueInOrder=function(iterable){
   }
   return result
 }
+
+function isIsogram(str){
+  str = str.toLowerCase();
+  let charSet = new Set();
+  for(let char of str){
+    if(charSet.has(char)){
+      return false
+    }
+    charSet.add(char)
+  }
+  return true;
+}
