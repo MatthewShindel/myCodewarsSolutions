257be65af21372923a4ff9187e2f250cfb6c40dd
MatthewShindel <matthew@shindel.com>
2024-08-06 12:35:14 -0400

    added better firstNonRepeatingLetter

--- CodewarsSolutions.js
+++ CodewarsSolutions.js
@@ -689,3 +689,11 @@ function tribonacci(signature, n) {
   
   return result;
 }
+
+function firstNonRepeatingLetter(s) {
+  var t=s.toLowerCase();
+  for (var x=0;x<t.length;x++)
+    if(t.indexOf(t[x]) === t.lastIndexOf(t[x]))
+      return s[x];
+  return "";
+}
