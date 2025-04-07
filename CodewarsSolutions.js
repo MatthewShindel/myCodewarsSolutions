2d5ca99be2ce10a77bf60b2f0e7e0ed3f3a8d89f
MatthewShindel <matthew@shindel.com>
2025-04-07 15:18:03 -0400

    added isValidWalkand maxSequence'

--- CodewarsSolutions.js
+++ CodewarsSolutions.js
@@ -819,3 +819,30 @@ var countSheep = function (num){
 function solution(str){
   return str.split('').reverse().join('')
 }
+
+var maxSequence = function(arr){
+  let maxSum = 0;
+  let currentSum = 0;
+  for(let i = 0; i < arr.length; i++){
+    currentSum += arr[i];
+    if(currentSum < 0) currentSum = 0;
+    if(currentSum > maxSum) maxSum = currentSum;
+  }
+  return maxSum;
+}
+
+function isValidWalk(walk) {
+  if(walk.length !== 10)return false
+
+  let x = 0;
+  let y = 0;
+
+  for(let dir of walk){
+    if(dir === 'n') y += 1;
+    if(dir === 's') y -= 1;
+    if(dir === 'e') x += 1;
+    if(dir === 'w') x -= 1;
+  }
+  return x === 0 && y === 0;
+
+}
--- GreatFrontEndSolutions.js
+++ GreatFrontEndSolutions.js
@@ -11,3 +11,32 @@ export default function minBy(array, iteratee) {
 export default function functionLength(fn) {
   return fn.length
 }
+
+export default async function sleep(duration) {
+  return new Promise((resolve) => setTimeout(resolve,duration))
+}
+
+Array.prototype.square = function () {
+  const length = this.length;
+  const results = new Array(length);
+
+  for(let i = 0; i< length; i++) {
+    results[i] = this[i] * this[i];
+  }
+  return results;
+}; // this function is a prototype method that works on arrays, reading each elements in an array, and squares them
+//The array sets up a new array that's the same length as the original
+
+export default function setCancellableInterval(callback, delay, ...args) {
+  const timerID = setInterval(callback, delay, ...args);
+  return () => {
+    clearInterval(timerID)
+  }
+}
+
+export default function setCancellableTimeout(callback, delay, ...args) {
+  const timerId = setTimeout(callback,delay,...args);
+  return function cancel(){
+    clearTimeout(timerId)
+  }
+}
